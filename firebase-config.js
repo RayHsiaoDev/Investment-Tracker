636d4e1f2cd4943628205fc01dae7bb2d3465681
RayHsiaoDev <RayHsiaoDev@users.noreply.github.com>
2025-10-15 08:05:13 +0000

    deploy: b034ecb2d0a21f3deb18de423ffaa5c75f481e51

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.__firebase_config = {
+  apiKey: "AIzaSyCT_ZFAYhSaY3i6V2ufagT5LzWLE1kT3UU",
+  authDomain: "investment-tracker-32ea9.firebaseapp.com",
+  projectId: "investment-tracker-32ea9",
+  storageBucket: "investment-tracker-32ea9.firebasestorage.app",
+  messagingSenderId: "109341991623",
+  appId: "1:109341991623:web:c688dfac5c37f7306f94e2",
+  measurementId: "G-NVMVXB2TND"
+};
